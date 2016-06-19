6e852b13a8d5dbdb0a7e0dd789c1ed44e9f3753a
chenjunkai <908346710@qq.com>
2016-06-19 14:23:30 +0800

    完善了DAO模块中实体类的属性及方法.

--- DAO/src/com/scutcms/DAO/entity/Employee.java
+++ DAO/src/com/scutcms/DAO/entity/Employee.java
@@ -8,13 +8,112 @@ import java.util.Date;
 
 
 public class Employee {
+    /**
+     * 员工工作编号
+     */
     private String employeeId;
+    /**
+     * 员工姓名
+     */
     private String name;
+    /**
+     * 员工性别
+     */
     private String sex;
-    private String sfzh;
+    /**
+     * 年龄
+     */
     private int age;
+    /**
+     * 住址
+     */
     private String address;
+    /**
+     * 联系方式
+     */
     private String telephone;
+    /**
+     * 入职时间
+     */
     private Date enter_time;
+    /**
+     * 岗位
+     */
     private String positionNname;
+    /**
+     * 身份证号
+     */
+    private String sfzh;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getPositionNname() {
+        return positionNname;
+    }
+
+    public void setPositionNname(String positionNname) {
+        this.positionNname = positionNname;
+    }
+
+    public Date getEnter_time() {
+        return enter_time;
+    }
+
+    public void setEnter_time(Date enter_time) {
+        this.enter_time = enter_time;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSfzh() {
+        return sfzh;
+    }
+
+    public void setSfzh(String sfzh) {
+        this.sfzh = sfzh;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
